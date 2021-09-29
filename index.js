9183e656ad7271faf32a2e446b976b5392f0fc3a
Tim Huerne <timothee.huerne@gmail.com>
2021-09-29 07:35:15 +0200

    port function updated

--- index.js
+++ index.js
@@ -98,6 +98,10 @@ app.get('/account/all', function (req, res) {
     });
 });
 
-var port = 3000;
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 app.listen(port);
 console.log('Running on port: ' + port);
